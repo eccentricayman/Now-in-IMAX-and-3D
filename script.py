88c4f488fda2365503b6093c3009f2fc672aead5
eccentricayman <aahmed3@stuy.edu>
2017-03-30 00:45:59 -0400

    idek

---
+++ script.py
@@ -0,0 +1,16 @@
+#ty @fierydarkwraith for the script :P
+import math
+
+with open("script", "w") as fd:
+
+    end = ""
+    for i in range(0, 60):
+        end += "clear\n"
+        end += "torus\n0 0 0 %d %d\n"%(i, 60 - i)
+        end += "apply\n"
+        if i < 10:
+            end += "save\n anim/00%d.png\n"%(i)
+        else:
+            end += "save\n anim/0%d.png\n"%(i)
+    print end
+    fd.write(end)
